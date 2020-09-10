592b94a229bdae32e81210f7fc63acbe5f0dbcee
brunamarques <bruna99stefany@gmail.com>
2020-09-10 14:43:18 -0300

    funçao divide vetor atualizada

--- samplesort_seq.c
+++ samplesort_seq.c
@@ -10,13 +10,14 @@
 int num_threads;
 int tam_vet;
 int *vetor;
+int **vet_aux;
 
 typedef struct indice{
     int inicio;
     int fim;
 } indice;
 
-indice *vet_indices;
+// indice *vet_indices;
 
 
 
@@ -43,21 +44,53 @@ void preencheVet(){
     int x;
     vetor = (int *)malloc(sizeof(int *) * tam_vet);
     // int vetor[tam_vet];
+    srand(time(NULL));
+
     for(int i = 0; i<tam_vet; i++){
         vetor[i] = rand() % (tam_vet- 1);
     }
-}
-
-void divide_vetor(){
-    vet_indices = (int *)malloc(sizeof(int *) * num_threads);
-    int aux = (tam_vet/num_threads) -1;
-    for(int i = 0; i < tam_vet; i+= (tam_vet/num_threads)){
-        vet_indices->inicio = i;
-        vet_indices->fim = aux;
-        aux+= (tam_vet/num_threads);
-
+    printf("VETOR INICIAL: ");
+    for(int i = 0; i<tam_vet; i++){
+        printf(" %d ", vetor[i]);
     }
 }
+
+// void divide_vetor(){
+//     vet_indices = (int *)malloc(sizeof(int *) * num_threads);
+//     int aux = (tam_vet/num_threads) -1;
+//     for(int i = 0; i < tam_vet; i+= (tam_vet/num_threads)){
+//         vet_indices[i].inicio = i;
+//         vet_indices[i].fim = aux;
+//         aux+= (tam_vet/num_threads);
+
+//     }
+// }
+void divide_vetor(){
+    vet_aux = (int **)malloc(num_threads * sizeof(int *));
+    int k = 0;
+    for(int i = 0; i < num_threads; i++ ){
+        vet_aux[i] = (int*) malloc(tam_vet/num_threads * sizeof(int));
+        for(int j = 0; j< tam_vet/num_threads; j++){
+            printf("J: %d ", j);
+            vet_aux[i][j] = vetor[k];
+            k++;
+        }
+    }
+    // printf("\n");
+    // for(int i = 0; i < num_threads; i++ ){
+    //     for(int k = 0; k< tam_vet/num_threads; k++){
+    //         printf(" %d ",vet_aux[i][k]);
+    //     }
+    //     printf("\n");
+    // }
+}
+
+// void pivo(){
+//     int num_pivo = num_threads -1;
+//     for(int i = 0; i<tam_vet; i++){
+//         vet_indices[i]
+//     }
+// }
 
 int main(int argv, char **argc){
     parametros(argv, argc);
