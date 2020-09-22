9a1460aec35f90d1764cdb26a9f188ba1d297317
brunamarques <bruna99stefany@gmail.com>
2020-09-22 17:56:10 -0300

    codigo paralelo indo

--- samplesort_par.c
+++ samplesort_par.c
@@ -1,48 +1,155 @@
-#include <pthread.h>
+#include <pthread.h> 
+#include <math.h>
 #include <string.h>
-#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <semaphore.h>
 #include <ctype.h>
+#include <getopt.h>
 
 #define MAX 16
 
 FILE *arq;
-int err;
 int num_threads;
 int tam_vet;
-int mediana;
+float mediana;
 int num_pivo;
 int *vetor;
+int *vetor_pivo;
+int *vet_pivoaux;
+int **vet_aux;
+int *vet_ord;
+int y;
+pthread_mutex_t lock;
 
-typedef struct {
-    pthread_mutex_t mutex;
-    sem_t waitsem;
-    int nthreads, current;
-}barrier_t, *ptr_barrier_t;
 
-void barrier_init(ptr_barrier_t pbarrier, int nt) {
-    pbarrier->nthreads = nt;
-    pbarrier->current = 0;
-    pthread_mutex_init(&(pbarrier->mutex), NULL);
-    sem_init(&(pbarrier->waitsem), 0, 0);
+
+
+// typedef struct {
+//     pthread_mutex_t mutex;
+//     sem_t waitsem;
+//     int nthreads, current;
+// }barrier_t, *ptr_barrier_t;
+
+// void barrier_init(ptr_barrier_t pbarrier, int nt) {
+//     pbarrier->nthreads = nt;
+//     pbarrier->current = 0;
+//     pthread_mutex_init(&(pbarrier->mutex), NULL);
+//     sem_init(&(pbarrier->waitsem), 0, 0);
+// }
+
+// void barrier(ptr_barrier_t pbarrier) {
+//     int i;
+//     pthread_mutex_lock(&(pbarrier->mutex));
+//     pbarrier->current++;
+//     if(pbarrier->current < pbarrier->nthreads) {
+//         pthread_mutex_unlock(&(pbarrier->mutex));
+//         sem_wait(&(pbarrier->waitsem));
+//     }else{
+//         for(i=0; i<(pbarrier->nthreads - 1); i++)
+//             sem_post(&(pbarrier->waitsem));
+//         pbarrier->current = 0;
+//         pthread_mutex_unlock(&(pbarrier->mutex));
+//     }
+// }
+
+pthread_barrier_t barrier;
+
+int comparador(const void *a, const void *b)
+{
+    return (*(int *)a - *(int *)b);
 }
 
-void barrier(ptr_barrier_t pbarrier) {
-    int i;
-    pthread_mutex_lock(&(pbarrier->mutex));
-    pbarrier->current++;
-    if(pbarrier->current < pbarrier->nthreads) {
-        pthread_mutex_unlock(&(pbarrier->mutex));
-        sem_wait(&(pbarrier->waitsem));
-    }else{
-        for(i=0; i<(pbarrier->nthreads - 1); i++)
-            sem_post(&(pbarrier->waitsem));
-        pbarrier->current = 0;
-        pthread_mutex_unlock(&(pbarrier->mutex));
+void preencheVet()
+{
+    vetor = (int *)malloc(sizeof(int *) * tam_vet);
+    srand(time(NULL));
+
+    for (int i = 0; i < tam_vet; i++)
+    {
+        vetor[i] = rand() % (tam_vet);
     }
+}
+
+void pivo(int id)
+{
+    int x = tam_vet % num_threads;
+    int k = 0;
+
+    for (int i = id; i <= id; i++)
+    {
+        if (x != 0)
+        {
+            for (int j = 0; j < (tam_vet / num_threads) + 1; j += mediana)
+            {
+                vet_pivoaux[k] = vet_aux[i][j];
+                k++;
+            }
+            x--;
+        }
+        else
+        {
+            for (int j = 0; j < (tam_vet / num_threads); j += mediana)
+            {
+                vet_pivoaux[k] = vet_aux[i][j];
+                k++;
+            }
+        }
+    }
+    k = 0;
+
+    qsort(vet_pivoaux, num_threads * num_threads, sizeof(int), comparador);
+
+    for (int j = 0; j < num_threads * num_threads; j += num_threads)
+    {
+        vetor_pivo[k] = vet_pivoaux[j];
+        k++;
+    }
+}
+
+void *divide_vetor(int id)
+{
+    printf("pqqqqqqqqqq");
+    printf("\n ID %d \n", id);
+    int comeco = id * num_threads;
+    int x = tam_vet % num_threads;
+    printf("começo %d ", comeco);
+    for (int i = id; i <= id; i++)
+    {
+        printf("FOR ID : %d " ,id);
+        if (x != 0)
+        {
+            vet_aux[i] = (int *)malloc((tam_vet / num_threads) + 1 * sizeof(int));
+            x--;
+
+            for (int j = 0; j < (tam_vet / num_threads) + 1; j++)
+            {
+                vet_aux[i][j] = vetor[y];
+                pthread_mutex_lock(&lock);
+                y++;
+                pthread_mutex_unlock(&lock);
+            }
+            qsort(vet_aux[i], (tam_vet / num_threads) + 1, sizeof(int), comparador);
+        }
+        else
+        {
+            vet_aux[id] = (int *)malloc((tam_vet / num_threads) * sizeof(int));
+
+            for (int j = 0; j <  (tam_vet / num_threads); j++)
+            {
+                vet_aux[id][j] = vetor[y];
+                pthread_mutex_lock(&lock);
+                y++;
+                pthread_mutex_unlock(&lock);
+            }
+            qsort(vet_aux[id], tam_vet / num_threads, sizeof(int), comparador);
+        }
+   }
+   pthread_barrier_wait (&barrier);
+   //y = 0;
+   //pivo(id);
 }
 
 void parametros(int t, char **args)
@@ -97,16 +204,40 @@ void parametros(int t, char **args)
 
 int main(int argv, char **argc)
 {
+    vet_aux = (long long **)malloc(num_threads * sizeof(long long *));
+    vet_pivoaux = (long long *)malloc(sizeof(long long *) * (num_threads * num_threads));
+    vetor_pivo = (long long *)malloc(sizeof(long long *) * (num_threads));
+    printf("AAAAA %f ", floor((tam_vet / num_threads) / num_threads));
+    //mediana = floor((tam_vet / num_threads) / num_threads);
+
+    y = 0;
+    int err;
     pthread_t tid[MAX];
     parametros(argv, argc);
-    divide_vetor();
-    pivo();
-    ordenacao();
-
+    // divide_vetor();
+    // pivo();
+    // ordenacao();
+    pthread_barrier_init (&barrier, NULL, num_threads);
+   
     for( int i=0; i<num_threads; i++){
-        err = pthread_create(&tid[i], NULL, (void*) "função", i); //o pthread_create que armazena um valor no id
+        err = pthread_create(&tid[i], NULL, (void*) divide_vetor, i); //o pthread_create que armazena um valor no id
+        
     }
     for (int i=0; i< num_threads; i++){
         err = pthread_join(tid[i], NULL); //o join junta todas as threads de volta, trazendo o result final
+    }
+
+     printf("\n");
+    for(int j = 0; j< tam_vet; j++)
+        printf(" %d ", vetor[j]);
+
+    printf("\n---------------\n");
+    for(int i = 0; i< num_threads; i++){
+        for (int j = 0; j < tam_vet/num_threads; j++)
+        {
+            printf(" %d ", vet_aux[i][j]);
+        }
+        printf("\n");
+        
     }
 }
